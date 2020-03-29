2130ae218805b6360a5df38c7a235f220c974bd3
Ricky Ting <Ricky.Ting@outlook.com>
2020-03-29 12:55:14 +0800

    Add hash func

--- Lab/Code/symbol.c
+++ Lab/Code/symbol.c
@@ -1,1 +1,12 @@
 #include "symbol.h"
+
+unsigned int hash_pjw(char* name) {
+    unsigned int val = 0, i;
+    for (; *name; ++name) {
+        val = (val<<2) + *name;
+        if (i = val & ~0x3fff)
+            val = (val ^ (i >> 12) & 0x3fff);
+    }
+    return val;
+}
+
