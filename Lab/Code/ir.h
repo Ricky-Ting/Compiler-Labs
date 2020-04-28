784e568dc24a5f516bf372b58d1eada3b0760472
Ricky Ting <Ricky.Ting@outlook.com>
2020-04-28 20:32:43 +0800

    Add some def

---
+++ Lab/Code/ir.h
@@ -0,0 +1,70 @@
+#ifndef IR_H
+#define IR_H
+
+#include "Tree.h"
+#include "symbol.h"
+
+#define TYPE_INT 0
+#define TYPE_FLOAT 1
+
+typedef struct Operand_ Operand_t;
+typedef Operand_t* Operand;
+
+typedef struct InterCode_ InterCode_t;
+typedef InterCode_t* InterCode;
+
+typedef struct InterCodes_ InterCodes_t;
+typedef InterCodes_t* InterCodes;
+
+
+struct Operand_ {
+    enum { VARIABLE, CONSTANT, ADDRESS } kind;
+    union {
+        int var_no;
+        int value;
+    } u;
+};
+
+struct InterCode_ {
+    enum { LABEL, FUNCTION, ASSIGN, ADD, SUB, MUL, DIV, ADDR, DEREF, REF_ASSIGN, 
+            GOTO, CONJMP, RETURN, DEC, ARG, CALL, PARAM, READ, WRITE  } kind;
+    union {
+        struct { Operand right, left; } assign;
+        struct { Operand result, op1, op2; } binop;
+        struct { int target; } label;
+        struct { int var_no; } unary;
+        struct { Operand op1, op2; int target; } conjmp;
+    } u;
+}
+
+struct InterCodes_ { InterCode code; struct InterCodes_ *prev, *next; };
+
+
+
+void sdt_init();
+
+/* High-level Definitions */
+
+
+
+/* Specifiers */
+
+
+/* Declarators */
+
+
+/* Statements */
+
+
+/* Local Definitions */
+
+
+
+/* Expressions */
+
+
+
+/* Terminator */
+
+
+#endif
