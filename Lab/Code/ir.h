482ee0044f7ee2871dd34cba1afe251f26237750
Ricky Ting <Ricky.Ting@outlook.com>
2020-05-02 14:04:28 +0800

    Before recode

--- Lab/Code/ir.h
+++ Lab/Code/ir.h
@@ -8,6 +8,7 @@
 #define TYPE_INT 0
 #define TYPE_FLOAT 1
 
+
 typedef struct Operand_ Operand_t;
 typedef Operand_t* Operand;
 
@@ -17,10 +18,10 @@ typedef InterCode_t* InterCode;
 typedef struct InterCodes_ InterCodes_t;
 typedef InterCodes_t* InterCodes;
 
-
 struct Operand_ {
     enum { VARIABLE, TEMP, FUNCT, LABEL, CONSTANT} kind;
     enum MODE mode;
+    enum { NORMAL, DEF, REF} print_mode;
     union {
         int var_no;
         int label;
@@ -45,54 +46,6 @@ struct InterCodes_ { InterCode code; struct InterCodes_ *prev, *next; };
 
 
 
-typedef struct expRet_ expRet_t;
-typedef expRet_t* expRet;
-
-struct expRet_ {
-    Type type;
-};
-
-void ir_init();
-
-/* High-level Definitions */
-void ir_Program(TreeNode_t* root, FILE* ir_file);
-void ir_ExtDefList(TreeNode_t *root);
-void ir_ExtDef(TreeNode_t *root);
-void ir_ExtDecList(TreeNode_t *root, Type baseType);
-Type ir_Specifier(TreeNode_t *root);
-
-/* Specifiers */
-Type ir_StructSpecifier(TreeNode_t* root);
-char* ir_OptTag(TreeNode_t* root);
-char* ir_Tag(TreeNode_t* root);
-
-/* Declarators */
-Symbol ir_VarDec(TreeNode_t* root, Type baseType, int size, int inStruct);
-Symbol ir_FunDec(TreeNode_t* root, Type retType, Symbol symbol);
-FieldList ir_VarList(TreeNode_t* root);
-FieldList ir_ParamDec(TreeNode_t* root);
-
-/* Statements */
-void ir_CompSt(TreeNode_t* root, Type retType);
-void ir_StmtList(TreeNode_t* root, Type retTyp);
-void ir_Stmt(TreeNode_t* root, Type retType);
-
-/* Local Definitions */
-FieldList ir_DefList(TreeNode_t* root, int inStruct, int offset);
-FieldList ir_Def(TreeNode_t* root, int inStruct, int offset);
-FieldList ir_DecList(TreeNode_t* root, Type baseType, int inStruct, int offset);
-FieldList ir_Dec(TreeNode_t* root, Type baseType, int inStruct, int offset);
-
-/* Expressions */
-Type ir_Exp(TreeNode_t* root, Operand place);
-void ir_Args(TreeNode_t* root);
-void ir_Cond(TreeNode_t* root, Operand label_true, Operand label_false);
-
-/* Terminator */
-char* ir_ID(TreeNode_t* root);
-int ir_TYPE(TreeNode_t* root);
-
-void printIR(InterCodes head, InterCodes tail, FILE* file);
-
 
 #endif
+
